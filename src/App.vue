0de48d62584f3c0be4b73b13a18cc32327d1f398
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-02-06 00:51:23 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {};
+</script>
+
+<template>
+  <h1>amogus</h1>
+</template>
+
+<style module lang="scss">
+@include _desktop {
+  h1 {
+    color: $red;
+  }
+}
+</style>
