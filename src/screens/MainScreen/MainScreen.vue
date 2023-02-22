e8243bb1bc6099e0228d819d9a0ab534531e26a6
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-02-23 00:00:24 +0300

    rewrite animations block

--- src/screens/MainScreen/MainScreen.vue
+++ src/screens/MainScreen/MainScreen.vue
@@ -1,49 +1,14 @@
 <script setup lang="ts">
-import { useMotion } from "@vueuse/motion";
-import { ref } from "vue";
-
-const targetEl = ref<HTMLDivElement>();
-
-const test = () => {
-  useMotion(targetEl, {
-    initial: {
-      opacity: 0,
-      y: 300,
-    },
-    enter: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        delay: 100,
-        type: "spring",
-        damping: 25,
-        mass: 0.5,
-      },
-    },
-  });
-};
+import AnimatedScreenContainer from "@/utils/animations/animatedScreenContainer.vue";
 </script>
 
 <template>
-  <div
-    class="target"
-    ref="targetEl"
-    @click="test"
-    :class="$style.mainContainer"
-  >
+  <AnimatedScreenContainer>
     <div :class="$style.test"></div>
-  </div>
+  </AnimatedScreenContainer>
 </template>
 
 <style module lang="scss">
-.mainContainer {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-}
-
 .test {
   width: 500px;
   height: 500px;
---
+++ src/utils/animations/AnimatedScreenContainer.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { useMotion } from "@vueuse/motion";
+import { ref } from "vue";
+
+const targetEl = ref<HTMLDivElement>();
+const test = () => {
+  useMotion(targetEl, {
+    initial: {
+      opacity: 0,
+      y: 300,
+    },
+    enter: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 100,
+        type: "spring",
+        damping: 25,
+        mass: 0.5,
+      },
+    },
+  });
+};
+</script>
+
+<template>
+  <div
+    class="target"
+    ref="targetEl"
+    @click="test"
+    :class="$style.mainContainer"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<style module lang="scss">
+.mainContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+}
+</style>
