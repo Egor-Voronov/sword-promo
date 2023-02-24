6d420f857d9b6dc7f9ab15d75f5d9e2845b903ae
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-02-24 14:47:16 +0300

    add debouncer and rewrite some code

--- src/modules/CurrentScreen/CurrentScreen.vue
+++ src/modules/CurrentScreen/CurrentScreen.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from "vue";
 import type { screensArray } from "@/modules/CurrentScreen/types/screensArray";
+import { useDebounceFn } from "@vueuse/core";
 import ScreenContainer from "@/components/ScreenContainer/ScreenContainer.vue";
 import { screenAppear } from "@/utils/animations/screenAppear";
 import Advantages from "../../screens/AdvantagesScreen/AdvantagesScreen.vue";
@@ -10,24 +11,43 @@ const screens: screensArray = [Main, Advantages];
 const currentScreen = ref(0);
 const targetEl = ref<HTMLDivElement>();
 
-const handleInput = (event: WheelEvent | KeyboardEvent) => {
-  const delta = event.type === "wheel" ? (event as WheelEvent).deltaY : 0;
-  const newScreen = currentScreen.value - Math.sign(delta);
+const handleScroll = (event: WheelEvent | KeyboardEvent) => {
+  let newScreen: number | undefined;
 
-  if (newScreen !== currentScreen.value) {
-    screenAppear(targetEl!);
+  if (event.type === "wheel") {
+    const delta = (event as WheelEvent).deltaY;
+    newScreen = currentScreen.value + Math.sign(delta);
+  }
+
+  if (event.type === "keydown") {
+    if ((event as KeyboardEvent).code === "ArrowUp") {
+      newScreen = currentScreen.value - 1;
+    } else if ((event as KeyboardEvent).code === "ArrowDown") {
+      newScreen = currentScreen.value + 1;
+    }
+  }
+
+  if (newScreen !== undefined && newScreen !== currentScreen.value) {
     currentScreen.value = Math.max(0, Math.min(newScreen, screens.length - 1));
+    screenAppear(targetEl!);
   }
 };
 
+const debouncedHandleScroll = useDebounceFn(
+  (event: WheelEvent | KeyboardEvent) => {
+    handleScroll(event);
+  },
+  400
+);
+
 onMounted(() => {
-  window.addEventListener("wheel", handleInput);
-  window.addEventListener("keydown", handleInput);
+  window.addEventListener("wheel", debouncedHandleScroll);
+  window.addEventListener("keydown", debouncedHandleScroll);
 });
 
 onUnmounted(() => {
-  window.removeEventListener("wheel", handleInput);
-  window.removeEventListener("keydown", handleInput);
+  window.removeEventListener("wheel", debouncedHandleScroll);
+  window.removeEventListener("keydown", debouncedHandleScroll);
 });
 </script>
 
