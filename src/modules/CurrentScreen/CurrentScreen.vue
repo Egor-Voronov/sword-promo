7c1c997ad013d5fee68796c1c23ec65703b7d1a9
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-02-24 04:41:33 +0300

    refactor code

--- src/modules/CurrentScreen/CurrentScreen.vue
+++ src/modules/CurrentScreen/CurrentScreen.vue
@@ -1,46 +1,22 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from "vue";
-import type { Ref } from "vue";
-import type { screensArray } from "./types/screensArray";
-import type { Range } from "./types/screensRange";
+import type { screensArray } from "@/modules/CurrentScreen/types/screensArray";
 import ScreenContainer from "@/components/ScreenContainer/ScreenContainer.vue";
 import { screenAppear } from "@/utils/animations/screenAppear";
 import Advantages from "../../screens/AdvantagesScreen/AdvantagesScreen.vue";
 import Main from "../../screens/MainScreen/MainScreen.vue";
 
-type T = Range<0, typeof screens.length>;
-
 const screens: screensArray = [Main, Advantages];
-let currentScreen = ref<T>(0);
-
+const currentScreen = ref(0);
 const targetEl = ref<HTMLDivElement>();
 
 const handleInput = (event: WheelEvent | KeyboardEvent) => {
-  if (event.type === "wheel") {
-    const newScreen =
-      currentScreen.value + ((event as WheelEvent).deltaY > 0 ? 1 : -1);
+  const delta = event.type === "wheel" ? (event as WheelEvent).deltaY : 0;
+  const newScreen = currentScreen.value - Math.sign(delta);
 
-    currentScreen.value = Math.min(
-      Math.max(newScreen, 0),
-      screens.length - 1
-    ) as T;
-
-    if (newScreen == currentScreen.value - 1) {
-      if (targetEl.value != null) {
-        screenAppear(targetEl!);
-      }
-    }
-  }
-
-  if (event.type === "keydown") {
-    if ((event as KeyboardEvent).code === "ArrowDown") {
-      const newScreen = currentScreen.value - 1;
-      currentScreen.value = Math.max(newScreen, 0) as T;
-    }
-    if ((event as KeyboardEvent).code === "ArrowUp") {
-      const newScreen = currentScreen.value + 1;
-      currentScreen.value = Math.min(newScreen, screens.length - 1) as T;
-    }
+  if (newScreen !== currentScreen.value) {
+    screenAppear(targetEl!);
+    currentScreen.value = Math.max(0, Math.min(newScreen, screens.length - 1));
   }
 };
 
--- src/utils/i18n/index.ts
+++ src/utils/i18n/index.ts
@@ -2,11 +2,6 @@ import { createI18n } from "vue-i18n";
 import en from "./locales/en.json";
 import ru from "./locales/ru.json";
 import { useCurrentLang } from "@/store/language";
-
-// export type Messages = {
-//   [key: string]: string | Messages;
-// }; oof fr hot
-
 export default createI18n({
   locale: useCurrentLang().value,
   fallbackLocale: "en",
