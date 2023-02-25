36bc0e90dafb85818bc8471485af378554e5a38b
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-02-26 00:23:20 +0300

    refactor code

---
+++ src/modules/FixedPanels/components/RotateImg/RotateImg.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useScreensState } from "@/store/screens";
+import rotateImg from "~/img/FixedPanels/rotate-y-axis.svg";
+
+const SCREENS_LENGTH = computed(() => useScreensState().SCREENS.value.length);
+const currentScreen = computed(() => useScreensState().currentScreen.value);
+</script>
+
+<template>
+  <img
+    :class="$style.img"
+    :src="rotateImg"
+    alt="rotate"
+    v-if="currentScreen !== SCREENS_LENGTH - 1"
+  />
+</template>
+
+<style lang="scss" module>
+img {
+  pointer-events: auto;
+  width: toRemMob(15);
+  height: toRemMob(15);
+
+  // pixel perfect styles
+  position: relative;
+  left: toRemMob(-7);
+
+  @include _desktop {
+    width: toRem(30);
+    height: toRem(30);
+  }
+}
+</style>
--- src/modules/FixedPanels/components/TopPanel.vue
+++ src/modules/FixedPanels/components/TopPanel.vue
@@ -1,28 +1,14 @@
 <script lang="ts" setup>
-import { ref, computed } from "vue";
 import FixedPanel from "../ui/FixedPanel.vue";
 import Socials from "./Socials/Socials.vue";
+import RotateImg from "./RotateImg/RotateImg.vue";
 import LangSwitcher from "./LangSwitcher/LangSwitcher.vue";
-import rotateImg from "~/img/FixedPanels/rotate-y-axis.svg";
-import { useScreensState } from "@/store/screens";
-
-let SCREENS_LENGTH = computed(() => useScreensState().SCREENS.value.length);
-let currentScreen = computed(() => useScreensState().currentScreen.value);
-
-let test = () =>
-  console.log(useScreensState().SCREENS.value, currentScreen.value, SCREENS_LENGTH.value);
 </script>
 
 <template>
   <fixed-panel :class="$style.topPanel">
     <Socials />
-    <img
-      :class="$style.img"
-      :src="rotateImg"
-      alt="rotate"
-      v-if="currentScreen !== SCREENS_LENGTH - 1"
-      @click="test"
-    />
+    <RotateImg />
     <LangSwitcher />
   </fixed-panel>
 </template>
@@ -31,20 +17,5 @@ let test = () =>
 .topPanel {
   top: 0;
   @extend %FixedPanelsBold;
-}
-
-img {
-  pointer-events: auto;
-  width: toRemMob(15);
-  height: toRemMob(15);
-
-  // pixel perfect styles
-  position: relative;
-  left: toRemMob(-7);
-
-  @include _desktop {
-    width: toRem(30);
-    height: toRem(30);
-  }
 }
 </style>
