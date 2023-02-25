c9297e6c70a4f6d0128c199f10a71bd5c83b577a
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-02-26 00:16:46 +0300

    add rotate img statement

--- src/modules/FixedPanels/components/TopPanel.vue
+++ src/modules/FixedPanels/components/TopPanel.vue
@@ -1,14 +1,28 @@
 <script lang="ts" setup>
+import { ref, computed } from "vue";
 import FixedPanel from "../ui/FixedPanel.vue";
 import Socials from "./Socials/Socials.vue";
 import LangSwitcher from "./LangSwitcher/LangSwitcher.vue";
 import rotateImg from "~/img/FixedPanels/rotate-y-axis.svg";
+import { useScreensState } from "@/store/screens";
+
+let SCREENS_LENGTH = computed(() => useScreensState().SCREENS.value.length);
+let currentScreen = computed(() => useScreensState().currentScreen.value);
+
+let test = () =>
+  console.log(useScreensState().SCREENS.value, currentScreen.value, SCREENS_LENGTH.value);
 </script>
 
 <template>
   <fixed-panel :class="$style.topPanel">
     <Socials />
-    <img :class="$style.img" :src="rotateImg" alt="rotate" />
+    <img
+      :class="$style.img"
+      :src="rotateImg"
+      alt="rotate"
+      v-if="currentScreen !== SCREENS_LENGTH - 1"
+      @click="test"
+    />
     <LangSwitcher />
   </fixed-panel>
 </template>
@@ -20,6 +34,7 @@ import rotateImg from "~/img/FixedPanels/rotate-y-axis.svg";
 }
 
 img {
+  pointer-events: auto;
   width: toRemMob(15);
   height: toRemMob(15);
 
