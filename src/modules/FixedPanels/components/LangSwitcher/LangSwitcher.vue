eea91b1c455ec6894cfc4243c6fdba25cd916031
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-02-27 00:10:55 +0300

    refactor code & add new logic

--- src/modules/FixedPanels/components/LangSwitcher/LangSwitcher.vue
+++ src/modules/FixedPanels/components/LangSwitcher/LangSwitcher.vue
@@ -1,17 +1,20 @@
 <script lang="ts" setup>
-import { computed } from "vue";
+import { computed, onMounted } from "vue";
 import { useCurrentLang } from "@/store/languages";
 import i18n from "@/utils/i18n";
 
 const currentLang = useCurrentLang();
 const locale = computed(() => i18n.global.locale.value);
+const html = document.querySelector("html");
 
 const switchLanguage = () => {
   const newLang = currentLang.value === "ru" ? "en" : "ru";
   currentLang.value = newLang;
   i18n.global.locale.value = newLang;
-  document.querySelector("html")?.setAttribute("lang", newLang);
+  html?.setAttribute("lang", newLang);
 };
+
+onMounted(() => html?.setAttribute("lang", i18n.global.locale.value));
 </script>
 
 <template>
