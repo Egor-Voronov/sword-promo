c86fedfa74d7434a129e883e8d83dcdd189b3946
Егор Воронов <vip.egor.voronov.2016@mail.ru>
2023-02-27 17:43:19 +0300

    create marquee

--- src/modules/HeroMarquee/HeroMarquee.vue
+++ src/modules/HeroMarquee/HeroMarquee.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 import Marquee from "@/components/Marquee/Marquee.vue";
 import { randomize } from "@/utils/randomizeFn";
-import { useScreensGood } from "@/store/goods/index";
+import { useScreensGood } from "@/store/goods";
+import mockups from "./mockups/mockups.json";
 </script>
 
 <template>
-  <Marquee :class="$style.heroMarquee"> </Marquee>
+  <Marquee :class="$style.heroMarquee">
+    {{ randomize(mockups[useScreensGood().currentGood.value]) }}
+  </Marquee>
 </template>
 
 <style lang="scss" module>
 .heroMarquee {
   width: 100vw;
+
+  @extend %HeroMarquee;
 }
 </style>
