ef65532e54d5e2d6dddb2d4e424efdfd9192a827
Егор Воронов <vip.egor.voronov.2016@mail.ru>
2023-02-27 16:41:55 +0300

    optimize main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import i18n from "@/utils/i18n";
-import { MotionPlugin } from "@vueuse/motion";
 
-createApp(App).use(MotionPlugin).use(i18n).mount("#app");
+createApp(App).use(i18n).mount("#app");
