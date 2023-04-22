b2b213a0bd55ab219e6dfa0bfe8571994c3f34f6
Egor Voronov <vip.egor.voronov.2016@mail.ru>
2023-04-22 17:58:12 +0300

    fix: made more consistent screens array typing

--- src/store/screens/index.ts
+++ src/store/screens/index.ts
@@ -1,11 +1,14 @@
 import { createGlobalState } from "@vueuse/core";
+import type { Component } from "vue";
 import { computed, ref } from "vue";
 import Hero from "@/screens/HeroScreen/TheHeroScreen.vue";
 import Advantages from "@/screens/AdvantagesScreen/TheAdvantagesScreen.vue";
 import Preorder from "@/screens/PreorderScreen/ThePreorderScreen.vue";
 
 export const useScreensState = createGlobalState(() => {
-  const SCREENS = computed(() => [Hero, Advantages, Preorder]);
+  const SCREENS = computed(
+    () => <Readonly<Component[]>>[Hero, Advantages, Preorder]
+  );
   const currentScreen = ref(0);
   const targetEl = ref<HTMLDivElement>();
 
